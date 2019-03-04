4b05341bbd6626b21c2d14a2f53a915b9e72e4e4
thfaucheux1 <thfaucheux1@iutclinfa1809l.iut.local>
2019-03-04 17:18:20 +0100

    Création de la classe personnage et debut du changement de position au changement de valeur du gyro mais erreur + architecture a revoir

--- app/src/main/java/com/iut/jumper/core/managers/SensorService.java
+++ app/src/main/java/com/iut/jumper/core/managers/SensorService.java
@@ -7,10 +7,16 @@ import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.util.Log;
 
+import com.iut.jumper.models.Personnage;
+
 public class SensorService implements SensorEventListener {
 
     private SensorManager sensorManager;
     private Sensor sensor;
+    private Personnage perso = null;
+    public Personnage getPerso() { return perso; }
+
+    public void setPerso(Personnage pperso) { this.perso = pperso; }
 
     public SensorService(Context context) {
         sensorManager = (SensorManager) context.getSystemService(Context.SENSOR_SERVICE);
@@ -27,7 +33,22 @@ public class SensorService implements SensorEventListener {
 
     @Override
     public void onSensorChanged(SensorEvent event) {
-        Log.d("JUMPER-SENSOR", String.valueOf(event.values[0])); // x
+        float x = event.values[0];
+        perso.setSpeedX(perso.getSpeedX() + x);
+        if(perso.getSpeedX() > perso.Vit_Max){
+            perso.setSpeedX(perso.Vit_Max);
+        }
+        float nposx = perso.getMyX() + perso.getSpeedX();
+        perso.setMyX(nposx);
+        if (event.values[0] > 0){
+            Log.d("Dirx", "Gauche");
+        }
+        else{
+            Log.d("Dirx", "Droite");
+        }
+        Log.d("Pos", String.valueOf(perso.getMyX()));
+        Log.d("Spd", String.valueOf(perso.getSpeedX()));
+        //Log.d("JUMPER-SENSOR", String.valueOf(event.values[0])); // x
         //Log.d("JUMPER-SENSOR", String.valueOf(event.values[1])); // y
         //Log.d("JUMPER-SENSOR", String.valueOf(event.values[2])); // z
     }
