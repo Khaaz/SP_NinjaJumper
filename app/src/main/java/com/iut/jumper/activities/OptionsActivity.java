915d575d33740217ecaab0a4f5e4d2466cea7b92
thfaucheux1 <thfaucheux1@iutclinfa1814l.iut.local>
2019-03-18 17:17:24 +0100

    les options sont sauvegardes lorsque l'on quitte l'application

--- app/src/main/java/com/iut/jumper/activities/OptionsActivity.java
+++ app/src/main/java/com/iut/jumper/activities/OptionsActivity.java
@@ -1,7 +1,10 @@
 package com.iut.jumper.activities;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.util.Log;
+import android.widget.CompoundButton;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.Switch;
@@ -10,24 +13,105 @@ import com.iut.jumper.R;
 
 public class OptionsActivity extends AActivity{
 
+    SharedPreferences options;
+    SharedPreferences.Editor editor;
+
+    Switch musique;
+    Switch difficulty;
+
+    RadioGroup skins;
+    RadioButton skinB;
+    RadioButton skinM;
+    RadioButton skinN;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         Log.d("JUMPER-Options", "onCreate");
         setContentView(R.layout.activity_options);
 
-        Switch musique = (Switch)findViewById(R.id.musique);
-        Switch difficulty = (Switch)findViewById(R.id.difficulty);
+        options = PreferenceManager.getDefaultSharedPreferences(this);
+        editor = options.edit();
 
-        RadioGroup skins = (RadioGroup)findViewById(R.id.skin);
-        RadioButton skinB = (RadioButton)findViewById(R.id.skinBase);
-        RadioButton skinM = (RadioButton)findViewById(R.id.skinMomie);
-        RadioButton skinN = (RadioButton)findViewById(R.id.skinNoel);
+        musique = (Switch)findViewById(R.id.musique);
+        difficulty = (Switch)findViewById(R.id.difficulty);
+
+        skins = (RadioGroup)findViewById(R.id.skin);
+        skinB = (RadioButton)findViewById(R.id.skinBase);
+        skinM = (RadioButton)findViewById(R.id.skinMomie);
+        skinN = (RadioButton)findViewById(R.id.skinNoel);
+
+
+        musique.setChecked(options.getBoolean("musique", true));
+        difficulty.setChecked(options.getBoolean("hardmode", false));
+
+        skinB.setChecked(options.getBoolean("base", true));
+        skinM.setChecked(options.getBoolean("momie", false));
+        skinN.setChecked(options.getBoolean("noel", false));
 
 
 
+        musique.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                changeValMusique(isChecked);
+            }
+        });
+
+        difficulty.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                changeValDiff(isChecked);
+            }
+        });
+
+        skinB.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                skinBase(isChecked);
+            }
+        });
+
+        skinM.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                skinMomie(isChecked);
+            }
+        });
+
+        skinN.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                skinNoel(isChecked);
+            }
+        });
 
     }
 
+    private void changeValMusique(boolean mus){
+        editor.putBoolean("musique", mus);
+        editor.apply();
+    }
+
+    private void changeValDiff(boolean dif){
+        editor.putBoolean("hardmode", dif);
+        editor.apply();
+    }
+
+    private void skinBase(boolean check){
+        editor.putBoolean("base", check);
+        editor.apply();
+    }
+
+    private void skinMomie(boolean check){
+        editor.putBoolean("momie", check);
+        editor.apply();
+    }
+
+    private void skinNoel(boolean check){
+        editor.putBoolean("noel", check);
+        editor.apply();
+    }
 
 }
