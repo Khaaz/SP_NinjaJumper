c0412a77ff7006be1a7d7c6cc9461d42f510c870
KhaaZ <louison.bellec@gmail.com>
2019-03-28 17:50:46 +0100

    Adjust accelerometer to work with tablets WIP #1

--- app/src/main/java/com/iut/jumper/core/managers/SensorService.java
+++ app/src/main/java/com/iut/jumper/core/managers/SensorService.java
@@ -6,6 +6,7 @@ import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.util.Log;
+import android.view.WindowManager;
 
 import com.iut.jumper.utils.Constants;
 
@@ -20,11 +21,15 @@ public class SensorService implements SensorEventListener {
 
     private double lastX = 0;
 
-     public SensorService(Context context, GameService gameService) {
-        this.gameService = gameService;
+    private int screenOrientation;
 
-        sensorManager = (SensorManager) context.getSystemService(Context.SENSOR_SERVICE);
-        sensor = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+     public SensorService(Context context, GameService gameService) {
+         this.gameService = gameService;
+
+         this.screenOrientation = ((WindowManager)context.getSystemService(Context.WINDOW_SERVICE)).getDefaultDisplay().getRotation();
+
+         sensorManager = (SensorManager) context.getSystemService(Context.SENSOR_SERVICE);
+         sensor = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
     }
 
     public void start() {
@@ -38,7 +43,8 @@ public class SensorService implements SensorEventListener {
     @Override
     public void onSensorChanged(SensorEvent event) {
 
-         double curX = event.values[0];
+        float[] values = this.adjustAccelOrientation(this.screenOrientation, event.values);
+        double curX = values[0];
 
         if (sensibility(curX)) {
             //Log.d("JUMPER-SENSOR", "SENSIBLE"); // x
@@ -63,6 +69,26 @@ public class SensorService implements SensorEventListener {
 
     private double calculateAcceleration(double curX) {
         return Math.abs(Constants.SENSOR_TO_SPEED_MULTIPLIER * curX);
+    }
+
+    // https://stackoverflow.com/questions/5877780/orientation-from-android-accelerometer/15552017
+    // http://developer.download.nvidia.com/tegra/docs/tegra_android_accelerometer_v5f.pdf
+    public float[] adjustAccelOrientation(int displayRotation, float[] eventValues) {
+        float[] adjustedValues = new float[3];
+
+        final int axisSwap[][] = {
+                {  1, -1, 0, 1 },     // ROTATION_0
+                { -1, -1, 1, 0 },     // ROTATION_90
+                { -1,  1, 0, 1 },     // ROTATION_180
+                {  1,  1, 1, 0 }
+        }; // ROTATION_270
+
+        final int[] as = axisSwap[displayRotation];
+        adjustedValues[0]  =  (float)as[0] * eventValues[ as[2] ];
+        adjustedValues[1]  =  (float)as[1] * eventValues[ as[3] ];
+        adjustedValues[2]  =  eventValues[2];
+
+        return adjustedValues;
     }
 
     @Override
